9a8077daa728e441f522a10d1caffdf260ebd80b
김민주 <hong00z@naver.com>
2024-11-18 11:00:23 +0900

    back 초기 데이터(영화, 장르) load

--- back/communities/views.py
+++ back/communities/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def example_view(request):
+    return HttpResponse("This is an example view!")
---
+++ back/init.py
@@ -0,0 +1,81 @@
+import requests
+import json
+
+# API 키 환경변수에서 읽어오기
+import os
+import environ
+# 환경변수를 불러올 수 있는 상태로 설정
+env = environ.Env(DEBUG=(bool, True))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# 읽어올 환경 변수 파일을 지정
+environ.Env.read_env(env_file = os.path.join(BASE_DIR, '.env'))
+TMDB_API_KEY = env('VITE_TMDB_API_KEY')
+
+def get_movie_datas():
+    total_data = []
+
+    for i in range(1, 10):
+        request_url = f"https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}&language=ko-KR&page={i}"
+        movies = requests.get(request_url).json()
+
+        for movie in movies['results']:
+            if movie.get('release_date', ''):
+                fields = {
+                    # 'movie_id': movie['id'],
+                    'title': movie['title'],
+                    'released_date': movie['release_date'],
+                    'popularity': movie['popularity'],
+                    'vote_avg': movie['vote_average'],
+                    'overview': movie['overview'],
+                    'poster_path': movie['poster_path'],
+                    'genres': movie['genre_ids']
+                }
+
+                data = {
+                    "pk": movie['id'],
+                    "model": "movies.movie",
+                    "fields": fields
+                }
+
+                total_data.append(data)
+
+    with open("movies/fixtures/movie_data.json", "w", encoding="utf-8") as w:
+        json.dump(total_data, w, indent=4, ensure_ascii=False)
+
+
+def get_genre_data():
+    total_data = []
+
+    request_url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={TMDB_API_KEY}"
+    genres = requests.get(request_url).json()
+
+    for genre in genres['genres']:
+        fields = {
+            # 'genre_id': genre['id'],
+            'name': genre['name'],
+        }
+
+        data = {
+            "pk": genre['id'],
+            "model": "movies.genre",
+            "fields": fields
+        }
+        total_data.append(data)
+
+    with open("movies/fixtures/genre_data.json", "w", encoding="utf-8") as w:
+        json.dump(total_data, w, indent=4, ensure_ascii=False)
+
+
+get_movie_datas()
+get_genre_data()
+
+'''
+movies/fixtures/ 만들고 
+
+python init.py 
+
+python manage.py migrate
+
+python manage.py loaddata genre_data.json movie_data.json
+
+'''
