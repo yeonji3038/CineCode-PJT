44188f4402e8abf62a305f2a220b2e872bf59867
김민주 <hong00z@naver.com>
2024-11-20 13:50:00 +0900

    signup 버튼 수정

--- back/movies/models.py
+++ back/movies/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    summary = models.TextField()
-    poster_path = models.TextField()
+    overview = models.TextField()
+    poster_path = models.TextField(blank=True, null=True)
     released_date = models.DateField()
     runtime = models.IntegerField()
+    popularity = models.FloatField(blank=True, null=True)
+    vote_avg = models.FloatField(blank=True, null=True)
 
